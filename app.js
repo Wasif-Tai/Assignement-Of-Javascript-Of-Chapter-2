a666b2d2ced3890656fdbd00ed64d948200c9683
Wasif Tai <muhammadwasiftai@gmail.com>
2025-10-28 21:31:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Question 1
+// var username = "wasifuiux"
+
+// Question 2
+var myName = "Muhammad Wasif Tai";
+alert("My Name Is: " + myName);
+
+// Question 3
+var message = "Hello World";
+alert(message);
+
+// Question 4
+var studentName = "Jhone Doe";
+var studentAge = "15 years old";
+var studentCourse = "Certified Mobile Application Development";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+// Question 5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// Question 6
+var email = "wasifui.ux@gmail.com";
+alert("My email address is: " + email);
+
+// Question 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
